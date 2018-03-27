889ebbb59bbb3521718b127cfa1b5d6a8fe88cef
José Miguel Sarasola <alosarjos@gmail.com>
2018-03-27 19:33:16 +0200

    Initial compass functionality

--- android/app/src/main/java/com/feelmusic/MainApplication.java
+++ android/app/src/main/java/com/feelmusic/MainApplication.java
@@ -3,6 +3,8 @@ package com.feelmusic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNSimpleCompassPackage;
+import com.sensors.RNSensorsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSimpleCompassPackage(),
+            new RNSensorsPackage(),
             new VectorIconsPackage()
       );
     }
--- src/containers/compass_container.js
+++ src/containers/compass_container.js
@@ -6,18 +6,56 @@ import { Container, Header, Content, Button, Icon, Text, Body, Title } from 'nat
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 
+import RNSimpleCompass from 'react-native-simple-compass';
+
 import { StyleSheet } from "react-native";
+
 
 class CompassContainer extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      currentDegree: 0,
+      zone: "One"
     };
   }
 
+  componentDidMount() {
+    const degree_update_rate = 3;
+    setInterval( () => {
+
+      RNSimpleCompass.start(degree_update_rate, (degree) => {
+        this.setState({
+          currentDegree : degree
+        })
+        RNSimpleCompass.stop();
+      });
+
+      var currentDegree = this.state.currentDegree;
+      var newZone = ""
+      if (currentDegree >= 0 && currentDegree < 90) {
+        newZone = "One";
+      } else if (currentDegree >= 90 && currentDegree < 180) {
+        newZone = "Two";
+      }
+      else if (currentDegree >= 180 && currentDegree < 270) {
+        newZone = "Three";
+      }
+      else {
+        newZone = "Four"
+      }
+      this.setState({
+        zone: newZone
+      })
+    },1000)
+
+
+  }
+
   render() {
+    
+    
     return (
         <Container>
             <Header>
@@ -26,6 +64,8 @@ class CompassContainer extends Component {
                 </Body>
             </Header>
             <Content padder contentContainerStyle={{}}>
+      <Text> {this.state.currentDegree} </Text>
+      <Text> Zone: {this.state.zone} </Text>
                 <Button onPress={() => Actions.pop() }>
                     <Icon name='arrow-back' />
                     <Text>Back</Text>
@@ -44,6 +84,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
   }
+
 }
 
 
