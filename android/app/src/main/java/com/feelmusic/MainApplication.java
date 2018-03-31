764b253156de2801fafc42556d7dcb8fc4990242
Juan Manuel Carrera García <juanmanuel@MacBook-Pro-de-Juan-Manuel.local>
2018-03-31 14:13:23 +0200

    merge

--- android/app/src/main/java/com/feelmusic/MainApplication.java
+++ android/app/src/main/java/com/feelmusic/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.zmxv.RNSound.RNSoundPackage;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
             new RNSoundPackage(),
             new VectorIconsPackage()
       );
